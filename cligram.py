dc9e1a369112d95298a31eb9fe333cb4eb99c0e2
PraaneshSelvaraj <praaneshselvaraj2003@gmail.com>
2022-10-02 20:05:52 +0530

    made as a class

--- cligram.py
+++ cligram.py
@@ -1,53 +1,81 @@
 import pyfiglet
-from includes import mainscreen
 
+class Cligram:
+    def __init__(self):
+        print(pyfiglet.figlet_format("cligram"))
+        self.data = None
 
-def create_account():
-    name =input("Enter a name : ")
-    pwd = input("Enter a secure password : ")
+        #getting user data from data.txt
+        with open("data.txt","r") as f:
+            self.data = f.readlines()
+        f.close()
     
-    with open("data.txt","r") as f:
-        data = f.readlines()
-    f.close()
+    def follow(self):
+        pass
 
-    #checks
-    for i in data:
-        if name in i:
-            print("Invalid username\n")
-            menu()
-    data.append(name+"~"+pwd)
+    def followers_list(self):
+        pass
 
-    with open("data.txt","w") as f:
-            f.writelines(data)
-    f.close()
+    def following_list(self):
+        pass
 
-def login():
-    name =input("Enter a name : ")
-    pwd = input("Enter a secure password : ")
-    with open("data.txt","r") as f:
-        data = f.readlines()
-    f.close()
+    def remove_following(self):
+        pass
 
-    for i in data:
-        user, pawd = i.split("~")
-        if user == name and pwd==pawd:
-            mainscreen.main_screen()
-            break
+    def friends(self):
+        print("1. Follow\n2. Following list\n3. Unfollow\n4.back")
 
-def menu():
-    print("1. Create Account\n2. Login\n3. Exit\n") 
-    opt = int(input("Choose an option : "))
+    def main_screen():
+        print("1. Friends\n2. Posts\n3. Back")
+        opt = int(input())
+        
+        if opt==1:
+            self.friends()
+        
+        elif opt==2:
+            self.posts()
+        
+        elif opt==3:
+            self.main_screen()
 
-    if(opt==1):
-        create_account()
-    elif opt==2:
-        login()
-    elif opt==3:
-        exit()
-    else:
-        print("Invalid option\n")
-        menu()
+    def login(Self):
+        self.name =input("Enter a name : ")
+        self.pwd = input("Enter a secure password : ")
 
-print(pyfiglet.figlet_format("cligram"))
-menu()
-    
+        for i in self.data:
+            user, pwd = i.split("~")
+            if user == name and pwd==pawd:
+                self.main_screen()
+                break
+
+    def create_account(self):
+        name =input("Enter a name : ")
+        pwd = input("Enter a secure password : ")
+        
+        for i in self.data:
+            if name in i:
+                print("Invalid username\n")
+                self.menu()
+        self.data.append(name+"~"+pwd)
+
+        with open("data.txt","w") as f:
+                f.writelines(self.data)
+        f.close()
+
+        self.menu()
+
+    def menu(self):
+        print("1. Create Account\n2. Login\n3. Exit\n") 
+        opt = int(input("Choose an option : "))
+
+        if opt==1:
+            self.create_account()
+        elif opt==2:
+            self.login()
+        elif opt==3:
+            exit()
+        else:
+            print("Invalid option\n")
+            self.menu()
+
+Cligram().menu()
